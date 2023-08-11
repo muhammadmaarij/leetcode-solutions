ec04bcefb19f68ad1881209407e2b97a8058ce4a
Muhammad Maarij <84044169+muhammadmaarij@users.noreply.github.com>
2023-08-11 23:26:01 +0500

    Create binary-search.js

---
+++ binary-search.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function (nums, target) {
+  let start = 0;
+  let end = nums.length;
+  while (start <= end) {
+    let mid = Math.floor((start + end) / 2);
+    if (nums[mid] === target) {
+      return mid;
+    } else if (nums[mid] < target) {
+      start = mid + 1;
+    } else {
+      end = mid - 1;
+    }
+  }
+  return -1;
+};
